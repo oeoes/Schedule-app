61eb70cfd113d1dd00665c29731fb1ece3a627d2
Oeoes Roy <oeoesroy@gmail.com>
2019-02-07 09:06:22 +0700

    adding new route

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,26 @@ Route::prefix('schedule')->group(function(){
     // Api
     Route::prefix('list')->group(function(){
         Route::get('/', 'AppController@courseList');
+        Route::get('/state', 'AppController@changeState');
     });
+    Route::get('/api/lecturers', 'AppController@apiLecturer')->name('lecturer');
 
     Route::get('/', 'AppController@index')->name('home');
+    Route::get('/lecturers', 'AppController@lecturer')->name('lecturer');
     Route::get('/sort/room/{id}/{state}', 'AppController@sortRoom')->name('sort.room');
 
     Route::prefix('dashboard')->group(function(){
         Route::get('/', 'DashboardController@home')->name('home.dashboard');
+        Route::get('/lecturers', 'DashboardController@lecturer')->name('lecturer.dashboard');
         Route::post('/add', 'DashboardController@addCourse')->name('add.course');
         Route::get('/sort/course', 'DashboardController@sortCourse')->name('sort.course');
         Route::post('/update', 'DashboardController@updateCourse')->name('update.course');
         Route::get('/delete/{id}', 'DashboardController@deleteCourse')->name('delete.course');
+
+        // Api 
+        Route::get('/api/lecturers', 'DashboardController@apiLecturer');
+        Route::get('/api/lecturers/{id}', 'DashboardController@apiLecturerSetState');
+        Route::get('/api/lecturers/add/{lecturer_name}', 'DashboardController@addLecturer');
 
         Route::get('/login', 'SessionController@login')->name('login');
         Route::post('/login/auth', 'SessionController@loginAuth')->name('login.auth');
